348913aa763d0873b22b6db605a337362d9086fd
DemonioS <alejandrowenzel@gmail.com>
2015-12-11 20:50:35 -0300

    documentos
    estante virtual

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+ 
+{
+    'name' : 'Estantes de Documentos',
+    'version' : '1.0',
+    'author' : 'Alejandro Wenzel',
+    'summary' : 'proyecto titulo',
+    'description' : 'estante virtual para guardar los archivos de la empresa',
+    'depends' : [
+            'base',
+            ],
+    'data' : [              
+            'documento_view.xml', 
+                ],
+    'installable' : True,
+    'aplication' : True,    
+}
